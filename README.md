1255fb5b652d6d8e3066fbedfbe97d9e40044a83
JohnIvan Chan <chanjohn@flip3.engr.oregonstate.edu>
2022-04-26 19:52:48 -0700

    Finished a rough version of the note taking app. Need to modify it so that contents aren't shown at the home-page Moreover, the contents will act as one large page that is editable and saves

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 This is a repository for the OSU course CS361
 source venv/bin/activate
+
+Code and html based off flask and python tutorials included below:
+
+Building a Python Note Application Using Flask and MongoDB
+https://www.section.io/engineering-education/building-a-simple-python-note-app-with-flask-and-mongodb/
+
+How To Make a Web Application Using Flask in Python 3
+https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
--- app.py
+++ app.py
@@ -1,21 +1,42 @@
 import os
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
 import datetime
-from flask import Flask
-from flask import request, Flask
-from flask import request,Flask,render_template,redirect
 from flask_pymongo import PyMongo
        
             
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/"
+app.config['SECRET_KEY'] = 'secret'
 mongo = PyMongo(app)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 
 @app.route('/')
 def home():
-    return render_template("/pages/home.html",homeIsActive=True,addNoteIsActive=False)
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template("/pages/home.html",homeIsActive=True,addNoteIsActive=False, posts=posts)
 
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('/pages/post.html',homeIsActive=True,addNoteIsActive=False,post=post)
 
 @app.route("/add-note", methods=['GET','POST'])
 def addNote():
@@ -27,46 +48,46 @@ def addNote():
 
         # get the fields data
         title = request.form['title']
-        description = request.form['description']
-        createdAt = datetime.datetime.now()
-
-
+        content = request.form['content']
+        created = datetime.datetime.now()
+        conn = get_db_connection()
+        conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                        (title, content))
+        conn.commit()
+        conn.close()
 
         return redirect("/")
 
-@app.route('/edit-note', methods=['GET','POST'])
-def editNote():
 
-    if request.method == "GET":
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
 
-        # get the id of the note to edit
-        noteId = request.args.get('form')
-
-
-
-        # direct to edit note page
-        return render_template('pages/edit-note.html')
-
-    elif request.method == "POST":
-
-        #get the data of the note
-        noteId = request.form['_id']
+    if request.method == 'POST':
         title = request.form['title']
-        description = request.form['description']
+        content = request.form['content']
 
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect("/")
 
+    return render_template('pages/edit-notes.html', post=post)
 
-        return redirect("/")
-
-@app.route('/delete-note', methods=['POST'])
-def deleteNote():
-
-    # get the id of the note to delete
-    noteId = request.form['_id']
-
-    # delete from the database
-
-    # redirect to home page
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect("/")
 
 if __name__ == "__main__":
